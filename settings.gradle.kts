2172c074aee195cd4d8305151207bb684539e91f
pablo <elmatecompany@gmail.com>
2025-02-15 16:14:12 +0100

    Primer commit

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+// Mapbox Maven repository
+
+
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+        }
+    }
+
+}
+
+rootProject.name = "ZBE"
+include(":androidZBE")
+include(":shared")
